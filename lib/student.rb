560f53d66e541e9c97e35a2e9dc9112aef8dc27e
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-01 01:17:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,12 @@
 class Student < User
-    
+
     attr_accessor :knowledge
 
     def initialize
         @knowledge = []
     end
 
-    
-    
-
+    def learn(subject)
+        knowledge << subject
+    end   
 end
